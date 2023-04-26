d38ecb74c9581f9b4cf1f4c8f83f2746773b6626
Chukwuemelie Obumse <emelieobumse100@gmail.com>
2023-04-26 23:50:46 +0000

    filtered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,7 +2,9 @@
 """filter logger"""
 
 import re
+import logging
 from typing import List, Tuple
+
 
 
 def splitter(message: str, separator: str) -> List:
@@ -31,3 +33,38 @@ def filter_datum(fields: List[str],
         for idx, val in enumerate(new_list):
             new_list[idx] = re.sub(pattern, replacement, val)
     return ";".join(new_list)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+    logging.basicConfig(level=logging.INFO, format=FORMAT)
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        # call to logging.Formatter, FORMAT is passed to
+        # tell logging.Formatter how records will be printed
+        self.fields= fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """returns a formatted and filtered record
+        - implemented call to the format method
+          of logging.Formatter, the parent class
+        
+        - filter_datum performed on the message to censor
+        personal details
+        
+        the censored details is returned
+        remember, format() method of logging.Formatter
+        is different from format passed to logging.Formatter
+        """
+        message = super().format(record)
+        filtered_record = filter_datum(self.fields,
+                                       self.REDACTION,
+                                       message,
+                                       self.SEPARATOR)
+        return filtered_record
