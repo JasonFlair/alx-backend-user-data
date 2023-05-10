8820b08a7a425660d86de9c723b826cf2e0cd670
Chukwuemelie Obumse <emelieobumse100@gmail.com>
2023-05-10 12:07:42 +0000

    tasks 0-15 done

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+"""Basic Flask App"""
+from flask import Flask, jsonify, make_response, request, abort
+from auth import Auth
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def simple_route():
+    """returns welcome message"""
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"],
+           strict_slashes=False)
+def users():
+    """registers a user"""
+    # retrieve email and password
+    try:
+        data = request.form
+        email = data.get("email")
+        password = data.get("password")
+    except Exception:
+        pass
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": "{}".format(email),
+                        "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"})
+
+
+@app.route("/sessions", methods=["POST"],
+           strict_slashes=False)
+def login():
+    """login function"""
+    try:
+        data = request.form
+        email = data.get("email")
+        password = data.get("password")
+    except Exception:
+        pass
+
+    # check if the login details are valid
+    valid_login = AUTH.valid_login(email, password)
+    if valid_login is True:
+        # create a session for that user
+        user_session_id = AUTH.create_session(email)
+        response = jsonify(
+            {"email": "{}".format(email), "message": "logged in"})
+        response.set_cookie("session_id", user_session_id)
+        return response
+    else:
+        abort(401)
+
+
+@app.route("/sessions", methods=["DELETE"],
+           strict_slashes=False)
+def logout():
+    """logout function"""
+    session_id = request.cookies.get("session_id")
+    user_with_session = AUTH.get_user_from_session_id(session_id)
+    if user_with_session is None:
+        abort(403)
+
+    # destroy the session by
+    # updating the corresponding user's session ID to None
+    AUTH.destroy_session(user_with_session.id)
+
+
+@app.route("/profile", methods=["GET"],
+           strict_slashes=False)
+def profile():
+    """profile function to respond to the GET /profile route."""
+    session_id = request.cookies.get("session_id")
+    print(session_id)
+    user_with_session = AUTH.get_user_from_session_id(session_id)
+    if user_with_session is None:
+        abort(403)
+    resp = make_response(jsonify({"email": user_with_session.email}, 200))
+    return resp
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
